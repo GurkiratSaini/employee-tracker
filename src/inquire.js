74401741c11794649e78a190609c54a01f604cf3
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-13 16:20:48 -0400

    set up dotenv, set up inquirer prompt

--- src/inquire.js
+++ src/inquire.js
@@ -1,1 +1,18 @@
 const inquirer = require('inquirer');
+
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'test',
+            choices: ['Option 1', 'Option 2']
+        }
+    ])
+        .then(inquirerData => {
+            if (inquirerData) {
+                console.log(inquirerData);
+            }
+        })
+}
+
+init();
