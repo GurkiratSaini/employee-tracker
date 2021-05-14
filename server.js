d680b96a3fe60e91c369d979ec15bbae0540526f
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-14 09:13:05 -0400

    cleaned up project directory structure

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PW,
-        database: 'employees'
+        database: process.env.DB_NAME
     },
     console.log('Connected to the employees database')
 );
