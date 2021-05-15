56773f8dfc53508d4259bec05a2f62032d2cb9e3
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-05-15 00:26:02 -0400

    finished app

--- db/queries/viewEmployeesByDepartment.sql
+++ db/queries/viewEmployeesByDepartment.sql
@@ -1,10 +1,13 @@
--- INCOMPLETE
-
-SELECT
+SELECT 
     IFNULL(dept.name, 'No Department') department_name,
     GROUP_CONCAT(
         e.first_name, " ", e.last_name
         SEPARATOR ", "
     ) employees
-FROM (employees e
-    LEFT JOIN department dept ON dept.id = e.)
+FROM (employee e
+    LEFT JOIN role ON e.role_id = role.id
+    LEFT JOIN department dept ON role.department_id = dept.id)
+GROUP BY
+    department_name
+ORDER BY
+    department_name;
--- db/queries/viewUtilizedBudgetOfDepartment.sql
+++ db/queries/viewUtilizedBudgetOfDepartment.sql
@@ -1,1 +1,10 @@
--- INCOMPLETE
+SELECT 
+    IFNULL(department.name, 'No department') department,
+    count(employee.id) employee_count,
+    sum(role.salary) utilized_budget
+FROM
+    employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+GROUP BY
+    role.title, role.salary, department.name
